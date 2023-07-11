a45ae196dd94a8c1556c0cefefca098c5b935c7c
Maneesh <maneeshmiracles@gmail.com>
2023-07-11 08:39:27 +0530

    day-02

--- Day-02/file01.py
+++ Day-02/file01.py
@@ -1,1 +1,5 @@
-print("Welcome to Python")
+# Python Start
+
+# Portable - We can move the source code from one platform to another
+# Platform Independent - We can move the intermediate code from one platform to another. 
+
