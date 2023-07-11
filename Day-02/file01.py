ce2808648d5ca80515233968b16a1ce309b08f6b
Maneesh <maneeshmiracles@gmail.com>
2023-07-11 12:32:03 +0530

    day-02

--- Day-02/file01.py
+++ Day-02/file01.py
@@ -1,5 +1,5 @@
-# Python Start
-
-# Portable - We can move the source code from one platform to another
-# Platform Independent - We can move the intermediate code from one platform to another. 
-
+# GIT - Distributed Version Control System
+# Git helps in keep track of the code
+# Git is used to collaborate on code
+# GitHub makes tools that use Git.
+# GitHub is the largest host of source code in the world, and has been owned by Microsoft since 2018.
