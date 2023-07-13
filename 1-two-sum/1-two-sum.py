59146a8829ced4ad6ac05e256802b02c931c9898
Maneesh Malepati <97582755+ManeeGit@users.noreply.github.com>
2023-07-13 21:39:10 +0530

    Delete 1-two-sum directory

--- 1-two-sum/1-two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target) :
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
