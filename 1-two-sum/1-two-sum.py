00ba0e6ca0b0c31933ba6fed9cbd6bd6436df409
Maneesh Malepati <97582755+ManeeGit@users.noreply.github.com>
2023-07-13 21:25:16 +0530

    Time: 46 ms (75.12%), Space: 13.9 MB (99.75%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums, target) :
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
