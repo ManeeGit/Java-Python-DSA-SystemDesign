ccd453c8d8f68cd5057d8a0e0bcf11586c591a22
Maneesh <maneeshmiracles@gmail.com>
2023-07-17 10:15:06 +0530

    Python-components-Strings

---
+++ Programming/Python/01-components.py
@@ -0,0 +1,24 @@
+# Python Components
+
+## Literals - any value which we are using
+''' 
+Int - 10 , -10 , 0 
+Float
+Complex - 10+20j
+None
+Boolean - True, False
+String
+
+'''
+## Identifiers 
+
+num = 10
+Num = 100
+print(num)
+print(Num)
+
+## Reserved Words
+## Expressions
+## Statements
+## Blocks & indentations
+## Comments
---
+++ Programming/Python/02-Strings.py
@@ -0,0 +1,9 @@
+# Strings in python are surrounded by either single quotation marks, or double quotation marks
+
+# Multi-Line Strings
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a) 
+
