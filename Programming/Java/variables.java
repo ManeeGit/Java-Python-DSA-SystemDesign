dddc376704362fbd666aaebb557b497ca80b78f0
Maneesh <maneeshmiracles@gmail.com>
2023-07-17 08:29:35 +0530

    Java-Variables-practice

---
+++ Programming/Java/variables.java
@@ -0,0 +1,26 @@
+package Programming.Java;
+
+public class variables {
+  public static void main(String[] args){
+
+    //Number
+    int num1,num2;
+    num1 = 10;
+    num2 = 20;
+    System.out.println(num1+num2);
+    //Boolean
+    System.out.println("Boolean values");
+    System.out.println(10>9);
+    //Float
+    float num3,num4;
+    num3 = 2.5f;
+    num4 = 3.4f;
+    System.out.println("The sum of num3 & num4 is "+(num3+num4));
+    //Char
+    char utf = 'M';
+    System.out.println(utf);
+    //String
+    String text = "Maneesh Malepati";
+    System.out.println("Founder of DeMyS : " + (text));
+  }
+}
